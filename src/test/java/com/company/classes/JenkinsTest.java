533ad4e39ff8aa0f9ab3557cdc9a0ddf24355574
Harjot Singh <harjot.singh@gspann.com>
2014-11-07 11:23:15 +0530

    simple Jenkins Test

---
+++ src/test/java/com/company/classes/JenkinsTest.java
@@ -0,0 +1,11 @@
+package com.company.classes;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+
+	@Test
+	public void textTest() {
+		System.out.println("First jenkins project");
+	}
+}
